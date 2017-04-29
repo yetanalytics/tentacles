23bb429d9db614f22ac23a96e2f75d515c29cec6
Henk Reder <henk@yetanalytics.com>
2017-04-29 11:06:21 -0400

    updated project.clj dependencies with ones on master
    [#144571573]

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.yetanalytics/tentacles "0.5.2"
+(defproject tentacles "0.5.3-SNAPSHOT"
   :description "A library for working with the Github API."
-  :url "https://github.com/yetanalytics/tentacles"
+  :url "https://github.com/Raynes/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.19"]
-                 [cheshire "5.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.2.0"]
+                 [cheshire "5.6.3"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.0"]
-                 [environ "1.0.0"]])
+                 [environ "1.1.0"]])
